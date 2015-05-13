11629a92eec8fb576bcbfbfa3286318bfe401a2c
Andrey Mikhaylov <and.mikhaylov@gmail.com>
2015-05-13 23:43:26 +0100

    Refactoring

--- js/public.js
+++ js/public.js
@@ -1,24 +1,24 @@
+//
+// Match browser helper script
+// (c) 2015 Andrej Mihajlov
+//
 
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
+    $('.wp-clanwars-list .maplist a').each(function () {
+        var href = $(this).attr('href');
+        var title = $(this).attr('title');
+        var p = href.indexOf('#');
+        var split = [ 150, 150 ];
 
-	$('.wp-clanwars-list .maplist a').each(function(){
-		var href = $(this).attr('href');
-		var title = $(this).attr('title');
+        if(p !== -1) {
+            split = href.substr(++p).split('x', 2);
+        }
 
-		var w, h;
+        var w = split[0];
+        var h = split[1];
 
-		var p = href.indexOf('#'), split = [150,150];
-
-		if(p != -1) {
-			split = href.substr(p+1).split('x', 2);
-		}
-
-		w = split[0];
-		h = split[1];
-
-		$(this).attr('title', '<div style="position: relative;"><img src="' + href + '" style="width: ' + w + 'px; height: ' + h + 'px;" /><div class="map-title">' + title + '</div></div>')
-			.tipsy({html: true})
-			.click(function(){ return false; });
-	});
-
+        $(this).attr('title', '<div style="position: relative;"><img src="' + href + '" style="width: ' + w + 'px; height: ' + h + 'px;" /><div class="map-title">' + title + '</div></div>')
+            .tipsy({html: true})
+            .on('click', function () { return false; });
+    });
 });
