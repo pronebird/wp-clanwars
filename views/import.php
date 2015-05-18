04500f58163b674de6c28b30a733540c51873765
Andrey Mikhaylov <and.mikhaylov@gmail.com>
2015-05-18 15:42:04 +0100

    Show games from API

--- views/import.php
+++ views/import.php
@@ -1,7 +1,63 @@
-<div class="wrap">
+<div class="wrap wp-clanwars-import-page">
 	<div id="icon-tools" class="icon32"><br></div>
 	<h2><?php _e('Import games', WP_CLANWARS_TEXTDOMAIN); ?></h2>
-	<p><?php _e('Import may take some time. Please do not refresh browser when in progress.', WP_CLANWARS_TEXTDOMAIN); ?></p>
+	<!-- <p><?php _e('Import may take some time. Please do not refresh browser when in progress.', WP_CLANWARS_TEXTDOMAIN); ?></p> -->
+
+	<div class="wp-filter">
+		<ul class="filter-links">
+			<li>
+				<a href="<?php echo admin_url('admin.php?page=wp-clanwars-import'); ?>" class="current"><?php _e('Popular', WP_CLANWARS_TEXTDOMAIN); ?></a>
+			</li>
+		</ul>
+		<form class="search-form">
+			<input type="search" name="q" value="" class="wp-filter-search" placeholder="<?php esc_attr_e(__('Search Gamepacks', WP_CLANWARS_TEXTDOMAIN)); ?>"
+		</form>
+	</div>
+
+	<ul class="wp-clanwars-gamepacks clearfix">
+
+	<?php foreach($popular as $game) : ?>
+		<li class="item">
+			<div class="top">
+				<div class="column-install"><a href="#" class="button">Install</a></div>
+				<h4>
+					<img src="<?php esc_attr_e($game['iconUrl']); ?>" alt="<?php esc_attr_e($game['title']); ?>" class="icon" />
+					<span class="game-title"><?php esc_html_e($game['title']); ?></span>
+				</h4>
+				
+				<ul class="maps">
+				<?php foreach($game['maps'] as $map) : ?>
+					<li>
+						<img class="screenshot" src="<?php esc_attr_e($map['imageUrl']); ?>" alt="<?php esc_attr_e($map['title']); ?>" />
+						<div class="title"><?php esc_html_e($map['title']); ?></div>
+					</li>
+				<?php endforeach; ?>
+				</ul>
+			</div>
+			<div class="bottom">
+				<div class="column-rating">
+					<div class="star-rating">
+						<div class="star star-full"></div>
+						<div class="star star-full"></div>
+						<div class="star star-full"></div>
+						<div class="star star-full"></div>
+						<div class="star star-empty"></div>
+					</div>
+					<span class="num-ratings"><?php echo sprintf( _x('(%d)', 'Number of ratings', WP_CLANWARS_TEXTDOMAIN), 599+$game['votes'] ); ?></span>
+				</div>
+				<div class="column-published">
+					<strong><?php _e('Published:', WP_CLANWARS_TEXTDOMAIN); ?></strong>
+					<span><?php esc_html_e( mysql2date(get_option('date_format'), $game['date'], true) ); ?></span>
+				</div>
+				<div class="column-downloaded"><?php echo sprintf( _nx('%d install', '%d installs', $game['downloads'], 'Number of downloads', WP_CLANWARS_TEXTDOMAIN ), 1000+$game['downloads'] ); ?></div>
+				<div class="column-author">
+					<strong><?php _e('Author:', WP_CLANWARS_TEXTDOMAIN); ?></strong> <?php esc_html_e($game['author']); ?>
+				</div>
+			</div>
+		</li>
+	<?php endforeach; ?>
+
+	</ul>
 
 	<form id="wp-cw-import" method="post" action="admin-post.php" enctype="multipart/form-data">
 
