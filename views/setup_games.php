22e28bd81652d47effb096d44860146bbc6306c7
Andrey Mikhaylov <and.mikhaylov@gmail.com>
2015-05-31 17:51:23 +0100

    Drop offline games from games on boarding

--- views/setup_games.php
+++ views/setup_games.php
@@ -11,31 +11,6 @@
 
 		<p><?php _e( 'Choose one of the options:', WP_CLANWARS_TEXTDOMAIN ); ?></p>
 
-		<?php if(!empty($import_list)) : ?>
-
-		<fieldset>
-			<p><label for="available"><input type="radio" name="import" id="available" value="available" checked="checked" /> <?php _e('Import from available games', WP_CLANWARS_TEXTDOMAIN); ?></label></p>
-
-			<ul class="available-games">
-
-			<?php foreach($import_list as $index => $game) : ?>
-
-				<li>
-					<label for="game-<?php esc_attr_e($index); ?>">
-						<input type="checkbox" name="items[]" id="game-<?php esc_attr_e($index); ?>" value="<?php esc_attr_e($index); ?>" /> <img src="<?php esc_attr_e(trailingslashit(WP_CLANWARS_IMPORTURL) . $game->icon); ?>" alt="<?php esc_attr_e($game->title); ?>" /> <?php esc_html_e($game->title); ?>
-						<?php if($game->is_installed) : ?>
-						<span class="description"><?php _e('installed', WP_CLANWARS_TEXTDOMAIN); ?></span>
-						<?php endif; ?>
-					</label>
-				</li>
-
-			<?php endforeach; ?>
-
-			</ul>
-		</fieldset>
-
-		<?php endif; ?>
-
 		<fieldset>
 			<p><label for="upload"><input type="radio" name="import" id="upload" value="upload" /> <?php _e('Upload previously saved game (ZIP file)', WP_CLANWARS_TEXTDOMAIN); ?></label></p>
 			<p><input type="file" name="userfile" /></p>
