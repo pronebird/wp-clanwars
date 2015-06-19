433307ccb6e9ad52501dbb577d0e115bbc0d0370
Andrej Mihajlov <and@codeispoetry.ru>
2015-06-19 16:29:09 +0200

    Add partials support to view class

--- classes/view.class.php
+++ classes/view.class.php
@@ -4,11 +4,28 @@ namespace WP_Clanwars;
 
 class View {
 
-	protected $template_path = '';
+	protected $template_file = '';
 	protected $helpers = array();
 
-	function __construct($name) {
-		$this->template_path = realpath(dirname(__FILE__) . '/../views/' . $name . '.php');
+	function __construct($template) {
+		$this->template_file = $template;
+
+		// use "use" keyword to pass $this in closures, php < 5.4 compatibility
+		$self = $this;
+
+		$this->helpers = array(
+			// use "use" keyword to pass $this in closures, php < 5.4 compatibility
+			'partial' => function($template, $args = array()) use ($self) {
+				$self->partial($template, $args);
+			}
+		);
+	}
+
+	function resolve($template) {
+		$viewDir = realpath(dirname(__FILE__) . '/../views');
+		$viewTemplate = $viewDir . '/' . $template . '.php';
+
+		return $viewTemplate;
 	}
 
 	function add_helper($name, $func) {
@@ -17,23 +34,50 @@ class View {
 		};
 	}
 
-	function render($context = array(), $echo = true) {
-		extract($this->helpers, EXTR_SKIP);
+	function partial($template, $args = array()) {
+		$partialTemplate = $this->resolve($template);
 
+		if(file_exists($partialTemplate)) {
+			// import view variables into scope
+			extract(get_object_vars($this), EXTR_SKIP);
+
+			if(is_array($args) && count($args) > 0) {
+				extract($args, EXTR_OVERWRITE);
+			}
+
+			extract($this->helpers, EXTR_OVERWRITE);
+
+			// include template
+			include($partialTemplate);
+		} else {
+			throw new Exception('Partial "' . $template . '" does not exist.');
+		}
+	}
+
+	function render($context = array(), $echo = true) {
 		if(is_array($context)) {
 			extract($context, EXTR_SKIP);
 		}
 
-		ob_start();
-		include($this->template_path);
-		$output = ob_get_clean();
+		extract($this->helpers, EXTR_SKIP);
+
+		$viewTemplate = $this->resolve($this->template_file);
+		$viewOutput = '';
+
+		if(file_exists($viewTemplate)) {
+			ob_start();
+			include($viewTemplate);
+			$viewOutput = ob_get_clean();
+		} else {
+			throw new Exception('Template "' . $this->template_file . '" does not exist.');
+		}
 
 		if($echo) {
-			echo $output;
+			echo $viewOutput;
 			return;
 		}
 
-		return $output;
+		return $viewOutput;
 	}
 
 };
