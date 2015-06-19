70ec089b3782bea8c97bd768037cf181e5c2e146
Andrej Mihajlov <and@codeispoetry.ru>
2015-06-19 16:30:35 +0200

    Add API::publish() method

--- classes/api.class.php
+++ classes/api.class.php
@@ -21,12 +21,60 @@ class API {
         return self::get_response_payload($response);
     }
 
+    static function publish($author, $email, $zip_file) {
+        if(!function_exists('curl_init')) {
+            return new WP_Error( 'api-error', 0, 'Unable to locate cURL extension.' );
+        }
+
+        $zip_file = realpath($zip_file);
+        $data = compact( 'author', 'email' );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, self::$api_url . 'games');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, 'Content-Type: multipart/form-data');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'WordPress/curl (via wp-clanwars)');
+
+        // use safe cURL uploads when possible
+        if( function_exists( 'curl_file_create' ) ) { // php 5.5+
+            // disable unsafe uploads (true is default in php 5.6+)
+            curl_setopt($ch, CURLOPT_SAFE_UPLOAD, true);
+
+            $data['file'] = curl_file_create($zip_file, 'application/zip', 'file');
+        }
+        else { // php 5.2+
+            // filter out attempts to upload files from server
+            // by prefixing text fields with @
+            // Remove @ from the beginning of each value.
+            array_walk($data, function (&$val) {
+                $val = preg_replace('#^@#i', '', trim( (string)$val ));
+            });
+
+            $data['file'] = '@' . $zip_file . ';type=application/zip';
+        }
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+        $response = curl_exec($ch);
+
+        if($response === false) {
+            $err = curl_error($ch);
+            $code = curl_errno($ch);
+            return new WP_Error( 'api-error-curl', $code, $err );
+        }
+
+        return self::get_response_payload($response);
+    }
+
     protected static function get_response_payload($response) {
         if(is_wp_error($response)) {
             return $response;
         }
 
-        $api_response = json_decode(wp_remote_retrieve_body($response));
+        $api_response = json_decode( is_string($response) ? $response : wp_remote_retrieve_body($response) );
 
         if(!$api_response->success) {
             $response_error = $api_response->error;
