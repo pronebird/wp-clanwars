57e7ed108b150e5aa0c4c3c20ff511d700202633
Andrey Mikhaylov <and.mikhaylov@gmail.com>
2015-07-13 14:04:22 +0300

    Add custom makepot to repo

---
+++ dev/pot.sh
@@ -0,0 +1,2 @@
+php tools/i18n/makepot.php wp-clanwars wp-clanwars wp-clanwars/langs/wp-clanwars.pot
+php tools/i18n/makepot.php wp-clanwars-countries wp-clanwars wp-clanwars/langs/wp-clanwars-countries.pot
