676a614cd2f4f3003ff6c76958db4200e807d260
Andrej Mihajlov <and@codeispoetry.ru>
2017-04-03 22:05:23 +0100

    Add uuid generator

--- classes/utils.class.php
+++ classes/utils.class.php
@@ -23,6 +23,16 @@ namespace WP_Clanwars;
 
 class Utils {
 
+	static function create_uuid() {
+		return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+				mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+				mt_rand( 0, 0xffff ),
+				mt_rand( 0, 0x0C2f ) | 0x4000,
+				mt_rand( 0, 0x3fff ) | 0x8000,
+				mt_rand( 0, 0x2Aff ), mt_rand( 0, 0xffD3 ), mt_rand( 0, 0xff4B )
+		);
+	}
+
 	static function get_wpmu_sites() {
 		global $wpdb;
 		if(function_exists('get_sites')) { // WP 4.6+
