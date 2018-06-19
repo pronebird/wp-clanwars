6bcfb35d0bcc02670abfa762807a76756fba839a
Andrej Mihajlov <and.mikhaylov@gmail.com>
2018-06-19 12:10:02 +0200

    Update localization and install script comment

--- dev/install.sh
+++ dev/install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+#
+# This script intalls the i18 'tools' and 'pot.sh' into `wp-content/plugins`.
+# I order to generate POT, cd to `wp-content/plugin` and run `pot.sh`
+#
+# Reference:
+# https://codex.wordpress.org/I18n_for_WordPress_Developers
+#
 
 cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd
 
